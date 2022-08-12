4752d2c3bd1743cfeb65ae0bfae6203e55a59a2e
Juan Cruz <juancruzpalau@gmail.com>
2022-08-12 10:04:24 -0300

    removed objects created with manu.js class due to change in food offer (only one type of dish, with different ingredients to choose, all described with simple text)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import { renderHeader, renderFooter, renderMain, resetMain } from "./pageLoad.js";
 import { renderHomeTab, renderMenuTab, renderContactTab } from "./tabs.js";
-import { createDish } from "./menu.js";
 
+export { resetMain };
 
 // From pageLoad.js
 renderHeader();
 renderMain();
 renderFooter();
 
-
 // From tabs.js
 renderHomeTab();
 renderMenuTab();
 renderContactTab();
 
-// From menu.js
-createDish('Tonkatsu', '$20', 'Coated slices of pork with panko (bread crumbs) fryed in oil, accompanied with rice.')
-createDish('Ramen', '$15', 'Noodles served in meat-based broth, flavored with soy sauce or miso, with sliced pork, nori (dried seaweed), menma, and scallions.')
-createDish('Nigiri pieces', '$20', 'Pieces of salmon placed on hand-pressed vinegared rice. A classic.')
 
-
-
-// Delete rendered content and replace it with content of the clicked tab.
+// Removes rendered content and replace it with content of the clicked tab.
 const headerItems = document.querySelectorAll('.headerItem');
 
 headerItems.forEach((item) => {
@@ -44,3 +37,4 @@ function switchTab(event) {
             break;
     }
 }
+
