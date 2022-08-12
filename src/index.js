a690dc136589be6723ce81c792956687d95b044c
Juan Cruz <juancruzpalau@gmail.com>
2022-08-12 09:29:52 -0300

    Adds function to switch tabs (removes rendered content and replaces it with the content of the selected tab)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('Welcome!');
+import { renderHeader, renderFooter, renderMain, resetMain } from "./pageLoad.js";
+import { renderHomeTab, renderMenuTab, renderContactTab } from "./tabs.js";
+import { createDish } from "./menu.js";
+
+
+// From pageLoad.js
+renderHeader();
+renderMain();
+renderFooter();
+
+
+// From tabs.js
+renderHomeTab();
+renderMenuTab();
+renderContactTab();
+
+// From menu.js
+createDish('Tonkatsu', '$20', 'Coated slices of pork with panko (bread crumbs) fryed in oil, accompanied with rice.')
+createDish('Ramen', '$15', 'Noodles served in meat-based broth, flavored with soy sauce or miso, with sliced pork, nori (dried seaweed), menma, and scallions.')
+createDish('Nigiri pieces', '$20', 'Pieces of salmon placed on hand-pressed vinegared rice. A classic.')
+
+
+
+// Delete rendered content and replace it with content of the clicked tab.
+const headerItems = document.querySelectorAll('.headerItem');
+
+headerItems.forEach((item) => {
+    item.addEventListener('click', switchTab);
+})
+
+function switchTab(event) {
+    switch (event.target.id) {
+        case 'home':
+            resetMain()
+            renderHomeTab()
+            break;
+        case 'menu':
+            resetMain()
+            renderMenuTab()
+            break;
+        case 'contact':
+            resetMain()
+            renderMenuTab()
+            break;
+    }
+}
